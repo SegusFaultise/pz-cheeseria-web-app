7835229ffa719ae3146bdaa77874580c5b7ad920
SegusFaultise <wilsonzakk8@gmail.com>
2024-10-20 16:09:54 +1000

    misc

--- pz-cheeseria-web-app/vite.config.ts
+++ pz-cheeseria-web-app/vite.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/pz-cheeseria-web-app/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
