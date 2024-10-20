7131f9f512e9689296e9ad6da647b1e84e8256e9
SegusFaultise <wilsonzakk8@gmail.com>
2024-10-20 13:21:45 +1000

    misc

--- pz-cheeseria-web-app/vue.config.js
+++ pz-cheeseria-web-app/vue.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/pz-cheeseria-web-app/pz-cheeseria-web-app/'
-    : '/'
+    publicPath: process.env.NODE_ENV === 'production' ? '/pz-cheeseria-web-app/' : '/'
 }
+  
