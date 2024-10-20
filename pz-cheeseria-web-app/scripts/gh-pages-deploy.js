2dbd4a75980e80b918e57c7da644fc4603592566
SegusFaultise <wilsonzakk8@gmail.com>
2024-10-20 12:52:32 +1000

    misc

--- pz-cheeseria-web-app/scripts/gh-pages-deploy.js
+++ pz-cheeseria-web-app/scripts/gh-pages-deploy.js
@@ -1,47 +1,27 @@
-import { execSync } from 'child_process';
-import fs from 'fs';
+/* eslint-disable no-console */
+const execa = import("execa");
+const fs = import("fs");
 
-// Path to the directory you want to deploy (the dist directory from your build)
-const BUILD_DIR = 'dist';
-
-// Ensure the build directory exists
-if (!fs.existsSync(BUILD_DIR)) {
-  console.error(`Build directory "${BUILD_DIR}" does not exist. Run the build first.`);
-  process.exit(1);
-}
-
-try {
-  // Clean up previous deployment
-  execSync('rm -rf .gh-pages');
-
-  // Clone the repository and switch to gh-pages branch if it exists, otherwise create it
-  execSync('git clone https://github.com/SegusFaultise/pz-cheeseria-web-app.git .gh-pages');
-  process.chdir('.gh-pages');
-  
+(async () => {
   try {
-    // Try checking out the gh-pages branch if it exists
-    execSync('git checkout gh-pages');
-  } catch (error) {
-    // Create the gh-pages branch if it doesn't exist
-    console.log('gh-pages branch does not exist. Creating a new one.', error);
-    execSync('git checkout --orphan gh-pages');
+    await execa("git", ["checkout", "--orphan", "gh-pages"]);
+    // eslint-disable-next-line no-console
+    console.log("Building started...");
+    await execa("npm", ["run", "build"]);
+    // await execa("yarn", ["build"]);
+    // Understand if it's dist or build folder
+    const folderName = fs.existsSync("dist") ? "dist" : "build";
+    await execa("git", ["--work-tree", folderName, "add", "--all"]);
+    await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
+    console.log("Pushing to gh-pages...");
+    await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
+    await execa("rm", ["-r", folderName]);
+    await execa("git", ["checkout", "-f", "main"]);
+    await execa("git", ["branch", "-D", "gh-pages"]);
+    console.log("Successfully deployed, check your settings");
+  } catch (e) {
+    // eslint-disable-next-line no-console
+    console.log(e.message);
+    process.exit(1);
   }
-
-  // Clear old files and copy the new build
-  execSync('rm -rf *');
-  execSync(`cp -r ../${BUILD_DIR}/* .`);
-
-  // Set Git config for this repository (required by the runner)
-  execSync('git config user.email "wilsonzakk8@gmail.com"');
-  execSync('git config user.name "SegusFaultise"');
-
-  // Commit and push changes
-  execSync('git add .');
-  execSync('git commit -m "Deploying new version"');
-  execSync('git push origin gh-pages');
-
-  console.log('Successfully deployed to GitHub Pages.');
-} catch (error) {
-  console.error('Deployment failed.', error);
-  process.exit(1);
-}
+})();
